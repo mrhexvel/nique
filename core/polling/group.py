0da67034b1fa8243697ad15f154d3cb420bf030f
mrhexvel <mr.hexvel@gmail.com>
2025-06-08 05:00:55 +0300

    ой...

--- core/polling/group.py
+++ core/polling/group.py
@@ -1,51 +1,7 @@
 from typing import Any, AsyncGenerator, Dict
 
-from loguru import logger
-
 from client.api import API
 from core.polling.base import LongPollProvider
-
-{
-    "group_id": 224391972,
-    "type": "message_new",
-    "event_id": "8bd6a82e64572cd9ca01de2dbadbca05256667b6",
-    "v": "5.199",
-    "object": {
-        "client_info": {
-            "button_actions": [
-                "text",
-                "vkpay",
-                "open_app",
-                "location",
-                "open_link",
-                "open_photo",
-                "callback",
-                "intent_subscribe",
-                "intent_unsubscribe",
-            ],
-            "keyboard": True,
-            "inline_keyboard": True,
-            "carousel": True,
-            "lang_id": 0,
-        },
-        "message": {
-            "date": 1749346530,
-            "from_id": 715616525,
-            "id": 0,
-            "version": 10010151,
-            "out": 0,
-            "fwd_messages": [],
-            "important": False,
-            "is_hidden": False,
-            "attachments": [],
-            "conversation_message_id": 147833,
-            "text": "/stats",
-            "is_unavailable": True,
-            "peer_id": 2000000005,
-            "random_id": 0,
-        },
-    },
-}
 
 
 class GroupLongPollProvider(LongPollProvider):
